e63c0da2d82cbc879a89c93608cbbb90deae335b
magatonman <magatonman@naver.com>
2024-07-03 02:00:47 +0900

    init

---
+++ psychicfrog-time/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Select from './component/Select.js'
+//import { Helmet } from "react-helmet";
+
+function App() {
+  return (
+    <div className="App">
+      <Select />
+    </div>
+  );
+}
+
+export default App;
